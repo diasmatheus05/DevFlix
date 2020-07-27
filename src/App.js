1f8223fdd4337e32a19bddc1a797369a376281b4
diasmatheus05 <diasmatheus05@gmail.com>
2020-07-27 13:50:51 -0300

    Primeiro dia: Fazendo a navBar e styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Menu from './components/Menu'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Menu />
     </div>
   );
 }
